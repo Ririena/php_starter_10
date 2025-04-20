fb14e9806b77f1327105d03500aa7f436f0c402e
Ririena <nerllysurina@gmail.com>
2025-04-20 17:14:13 +0700

    first commit

---
+++ resources/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-2xl text-blue-600"> Hello World</h1>
+            <div className="flex justify-center items-center ">
+                <button className="btn">Hello</button>
+            </div>
+        </div>
+    );
+}
